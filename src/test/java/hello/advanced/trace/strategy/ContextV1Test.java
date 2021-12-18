1a01784393348f16a215ef45875f817e0fcae06b
JunHwan Kim <nefertiri972@gmail.com>
2021-12-18 11:33:26 +0900

    Add strategy pattern to base example
    - 전략 패턴을 예제에 적용
    - 컨텍스트에 알고리즘의 구조를 정의(변하지 않는 부분)
    - 전략 인터페이스를 구현한 각각의 클래스에 알고리즘을 구현(변하는 부분)
    - 컨텍스트에서 전체 알고리즘을 수행할 때 변하는 부분은 전략 인터페이스 구현체에 위임
    - 클라이언트는 컨텍스트의 공통 메서드를 호출하여 사용

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,5 +1,9 @@
 package hello.advanced.trace.strategy;
 
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -31,4 +35,19 @@ public class ContextV1Test {
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
     }
+
+    /**
+     * 전략 패턴 사용
+     */
+    @Test
+    void strategyV1() {
+        Strategy strategy1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(strategy1);
+        context1.execute();
+
+        Strategy strategy2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategy2);
+        context2.execute();
+    }
+
 }
